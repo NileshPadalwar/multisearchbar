21ece109fc34fbb004b58cbb2656549dc3bf9e55
Nilesh <nileshpadalwar1998@gmail.com>
2024-05-02 09:50:35 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PillComponent } from './pill/pill.component';
+import {  NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserService } from './user.service';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    PillComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbTypeaheadModule
+  
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pill/pill.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-pill',
+  templateUrl: './pill.component.html',
+  styleUrls: ['./pill.component.css']
+})
+export class PillComponent {
+  @Input() data: any;
+  @Output() remove: EventEmitter<any> = new EventEmitter<any>();
+
+  onRemove(): void {
+    this.remove.emit(this.data);
+  }
+}
